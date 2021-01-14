39e3275886f4b8724d39d458a27ea510ef83a820
Nevzat Umut Demirseren <60689329+TheCrimsonNeV0@users.noreply.github.com>
2021-01-14 21:59:34 +0300

    .

--- Piece.java
+++ Piece.java
@@ -3,4 +3,5 @@ public abstract class Piece {
     public static final boolean BLACK = true;
     boolean color;
     abstract Position move(int from_x, int from_y, int to_x, int to_y) throws InvalidMoveException;
+    abstract boolean isPathAvailable(int from_x, int from_y, int to_x, int to_y);
 }
