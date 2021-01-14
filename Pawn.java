53434278840912ae81a38fe94bb046fceffbf084
Nevzat Umut Demirseren <60689329+TheCrimsonNeV0@users.noreply.github.com>
2021-01-14 21:16:47 +0300

    .

---
+++ Pawn.java
@@ -0,0 +1,11 @@
+public class Pawn extends Piece {
+    boolean didMove;
+
+    public Pawn(int color) {
+        didMove = false;
+    }
+
+    Position move(int from_x, int from_y, int to_x, int to_y) {
+        return null;
+    }
+}
--- Piece.java
+++ Piece.java
@@ -1,3 +1,6 @@
 public abstract class Piece {
+    public static final int WHITE = 0;
+    public static final int BLACK = 1;
+    int color;
     abstract Position move(int from_x, int from_y, int to_x, int to_y);
 }
