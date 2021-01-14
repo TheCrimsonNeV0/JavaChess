31ef03b52e88bab9974ec3ead2490ba576191ffe
Nevzat Umut Demirseren <60689329+TheCrimsonNeV0@users.noreply.github.com>
2021-01-14 22:37:12 +0300

    .

--- Pawn.java
+++ Pawn.java
@@ -1,3 +1,5 @@
+// Written by: Nevzat Umut Demirseren
+
 public class Pawn extends Piece {
     boolean hasMoved;
     boolean color;
@@ -9,7 +11,7 @@ public class Pawn extends Piece {
 
     Position move(int from_x, int from_y, int to_x, int to_y) throws InvalidMoveException {
         if (from_y == to_y) {
-            if (color) { //White
+            if (color == Piece.WHITE) { //White
                 if (from_x == to_x + 1) return new Position(to_x, to_y);
                 else if (from_x == to_x + 2) {
                     if (!hasMoved) return new Position(to_x, to_y);
@@ -26,5 +28,17 @@ public class Pawn extends Piece {
                 else throw new InvalidMoveException();
             }
         } else throw new InvalidMoveException();
+    }
+
+    @Override
+    boolean isPathAvailable(int from_x, int from_y, int to_x, int to_y) {
+        boolean isAvailable = true;
+        if (color == Piece.WHITE) { //White
+            if ()
+        }
+        else { //
+
+        }
+        return isAvailable;
     }
 }
