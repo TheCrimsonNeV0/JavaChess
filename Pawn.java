691bc0e2d6efbf58d86b6d9495240846ed6a51dc
Nevzat Umut Demirseren <60689329+TheCrimsonNeV0@users.noreply.github.com>
2021-01-14 21:40:30 +0300

    .

--- Pawn.java
+++ Pawn.java
@@ -1,11 +1,15 @@
 public class Pawn extends Piece {
     boolean didMove;
+    boolean hasMoved;
+    boolean color;
 
     public Pawn(int color) {
         didMove = false;
     }
 
-    Position move(int from_x, int from_y, int to_x, int to_y) {
-        return null;
+    Position move(int from_x, int from_y, int to_x, int to_y) throws InvalidMoveException {
+        if (from_y == to_y) {
+            return null;
+        } else throw new InvalidMoveException();
     }
 }
--- Piece.java
+++ Piece.java
@@ -2,5 +2,5 @@ public abstract class Piece {
     public static final boolean WHITE = false;
     public static final boolean BLACK = true;
     boolean color;
-    abstract Position move(int from_x, int from_y, int to_x, int to_y);
+    abstract Position move(int from_x, int from_y, int to_x, int to_y) throws InvalidMoveException;
 }
