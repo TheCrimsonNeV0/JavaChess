ce0e8d1c9c9cd39fa3912c7b59d4ab72a9319f4d
Nevzat Umut Demirseren <60689329+TheCrimsonNeV0@users.noreply.github.com>
2021-01-15 17:41:24 +0300

    .

--- Pawn.java
+++ Pawn.java
@@ -10,49 +10,29 @@ public class Pawn extends Piece {
     }
 
     void move(int from_x, int from_y, int to_x, int to_y) throws InvalidMoveException {
-        if (from_x == to_x) {
-            if (color == Piece.WHITE) { //White
-                if (from_y == to_y + 1) {
-                    if (isPathAvailable(from_x, from_y, to_x, to_y)) {
-                        hasMoved = true;
-                    }
-                    else throw new InvalidMoveException();
-                }
-                else if (from_y == to_y + 2) {
-                    if (!hasMoved) {
-                        if (isPathAvailable(from_x, from_y, to_x, to_y)) {
-                            hasMoved = true;
-                            Board.chessBoardArray[from_x][from_y] = null;
-                            Board.chessBoardArray[to_x][to_y] = this;
-                        }
-                        else throw new InvalidMoveException();
-                    }
-                    else throw new InvalidMoveException();
-                }
-                else throw new InvalidMoveException();
-            }
-            else { //Black
-                if (from_y == to_y - 1) {
-                    hasMoved = true;
-                    Board.chessBoardArray[from_x][from_y] = null;
-                    Board.chessBoardArray[to_x][to_y] = this;
-                }
-                else if (from_y == to_y - 2) {
-                    if (!hasMoved) {
-                        hasMoved = true;
-                        Board.chessBoardArray[from_x][from_y] = null;
-                        Board.chessBoardArray[to_x][to_y] = this;
-                    }
-                    else throw new InvalidMoveException();
-                }
-                else throw new InvalidMoveException();
-            }
-        } else throw new InvalidMoveException();
+        if (isPathAvailable(from_x, from_y, to_x, to_y)) {
+            Board.chessBoardArray[from_x][from_y] = null;
+            Board.chessBoardArray[to_x][to_y] = this;
+        }
+        else throw new InvalidMoveException();
+        this.hasMoved = true;
     }
 
     @Override
     boolean isPathAvailable(int from_x, int from_y, int to_x, int to_y) {
-        boolean isAvailable = true;
+        boolean isAvailable = false;
+        if (from_x == to_x) {
+            if (color == Piece.WHITE && from_y == to_y + 1) isAvailable = true;
+            else if (color == Piece.WHITE && from_y == to_y + 2 && !hasMoved) isAvailable = true;
+            else if (color == Piece.BLACK && from_y == to_y - 1) isAvailable = true;
+            else if (color == Piece.BLACK && from_y == to_y - 2 && !hasMoved) isAvailable = true;
+        } else if (Math.abs(from_x - to_x) == 1) {
+            if (color == Piece.WHITE && from_y == to_y + 1 && Board.chessBoardArray[to_x][to_y] != null && Board.chessBoardArray[to_x][to_y].color == Piece.BLACK) isAvailable = true;
+            else if (color == Piece.BLACK && from_y == to_y - 1 && Board.chessBoardArray[to_x][to_y] != null && Board.chessBoardArray[to_x][to_y].color == Piece.WHITE) isAvailable = true;
+        } else {
+            return false;
+        }
+
         if (color == Piece.WHITE) { //White
             for (int i = to_y; i <= from_y; i++) {
                 if (Board.chessBoardArray[to_x][i] == null) {
