0250e62431e630b6e6c2d6e75dd66e8027e3cf39
Nevzat Umut Demirseren <60689329+TheCrimsonNeV0@users.noreply.github.com>
2021-01-16 15:23:17 +0300

    isPathAvailable takes Board as parameter

--- Bishop.java
+++ Bishop.java
@@ -5,25 +5,13 @@ public class Bishop extends Piece{
 
     public Bishop(boolean color){
         this.color = color;
-        whiteImageIcon = new ImageIcon("Images/white_bishop.png"); // White bishop image
-        blackImageIcon = new ImageIcon("Images/black_bishop.png"); // Black bishop image
-    }
-
-    // Move method
-
-    @Override
-    void move(int from_x, int from_y, int to_x, int to_y) throws InvalidMoveException {
-        if (isPathAvailable(from_x, from_y, to_x, to_y)) {
-            Board.chessBoardArray[from_x][from_y] = null;
-            Board.chessBoardArray[to_x][to_y] = this;
-        }
-        else
-            throw new InvalidMoveException();
+        whiteImageIcon = new ImageIcon("Images/white_bishop.png");
+        blackImageIcon = new ImageIcon("Images/black_bishop.png");
     }
 
     // Simply iterate through the path to see if the specified path is available
 
-    boolean isPathAvailable(int from_x, int from_y, int to_x, int to_y){
+    boolean isPathAvailable(Board board, int from_x, int from_y, int to_x, int to_y){
         int dif_x = to_x - from_x;
         int dif_y = to_y - from_y;
 
@@ -32,33 +20,33 @@ public class Bishop extends Piece{
 
         if (dif_x > 0 && dif_y < 0) {        // NE
             for (int i = from_x+1, j = from_y-1; i < to_x; i++, j--){
-                if (Board.chessBoardArray[i][j] != null){
+                if (board.chessBoardArray[i][j] != null){
                     return false;
                 }
             }
         }
         else if (dif_x < 0 && dif_y < 0) {       // NW
             for (int i = from_x-1, j = from_y-1; i > to_x; i--, j--){
-                if (Board.chessBoardArray[i][j] != null){
+                if (board.chessBoardArray[i][j] != null){
                     return false;
                 }
             }
         }
         else if (dif_x > 0 && dif_y > 0){          // SE
             for (int i = from_x+1, j = from_y+1; i < to_x; i++, j++){
-                if (Board.chessBoardArray[i][j] != null){
+                if (board.chessBoardArray[i][j] != null){
                     return false;
                 }
             }
         }
         else if (dif_x < 0 && dif_y > 0) {         // SW
             for (int i = from_x-1, j = from_y+1; i > to_x; i--, j++){
-                if (Board.chessBoardArray[i][j] != null){
+                if (board.chessBoardArray[i][j] != null){
                     return false;
                 }
             }
         }
         // Check if the target color is different or the position is empty
-        return Board.chessBoardArray[to_x][to_y] == null || Board.chessBoardArray[to_x][to_y].color != this.color;
+        return board.chessBoardArray[to_x][to_y] == null || board.chessBoardArray[to_x][to_y].color != this.color;
     }
 }
