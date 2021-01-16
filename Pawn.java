c55220ef6d2a74ef351e4b5199285062cf0c2fda
Nevzat Umut Demirseren <60689329+TheCrimsonNeV0@users.noreply.github.com>
2021-01-16 15:26:16 +0300

    isPathAvailable takes Board as parameter

--- Pawn.java
+++ Pawn.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 
 public class Pawn extends Piece {
     boolean hasMoved;
-    boolean color;
 
     public Pawn(boolean color) {
         hasMoved = false;
@@ -13,38 +12,30 @@ public class Pawn extends Piece {
         blackImageIcon = new ImageIcon("Images/black_pawn.png");
     }
 
-    void move(int from_x, int from_y, int to_x, int to_y) throws InvalidMoveException {
-        if (isPathAvailable(from_x, from_y, to_x, to_y)) {
-            Board.chessBoardArray[from_x][from_y] = null;
-            Board.chessBoardArray[to_x][to_y] = this;
-        }
-        else throw new InvalidMoveException();
-        this.hasMoved = true;
-    }
-
     @Override
-    boolean isPathAvailable(int from_x, int from_y, int to_x, int to_y) {
+    boolean isPathAvailable(Board board, int from_x, int from_y, int to_x, int to_y) {
         boolean isAvailable = false;
         if (from_x == to_x) {
             if (color == Piece.WHITE && from_y == to_y + 1) {
-                if (Board.chessBoardArray[to_x][to_y] == null) isAvailable = true;
+                if (board.chessBoardArray[to_x][to_y] == null) isAvailable = true;
             }
             else if (color == Piece.WHITE && from_y == to_y + 2 && !hasMoved) {
-                if (Board.chessBoardArray[to_x][to_y - 1] == null && Board.chessBoardArray[to_x][to_y] == null) isAvailable = true;
+                if (board.chessBoardArray[to_x][to_y - 1] == null && board.chessBoardArray[to_x][to_y] == null) isAvailable = true;
             }
             else if (color == Piece.BLACK && from_y == to_y - 1) {
-                if (Board.chessBoardArray[to_x][to_y] == null) isAvailable = true;
+                if (board.chessBoardArray[to_x][to_y] == null) isAvailable = true;
             }
             else if (color == Piece.BLACK && from_y == to_y - 2 && !hasMoved) {
-                if (Board.chessBoardArray[to_x][to_y + 1] == null && Board.chessBoardArray[to_x][to_y] == null) isAvailable = true;
+                if (board.chessBoardArray[to_x][to_y + 1] == null && board.chessBoardArray[to_x][to_y] == null) isAvailable = true;
             }
         } else if (Math.abs(from_x - to_x) == 1) {
-            if (color == Piece.WHITE && from_y == to_y + 1 && Board.chessBoardArray[to_x][to_y] != null && Board.chessBoardArray[to_x][to_y].color == Piece.BLACK) isAvailable = true;
-            else if (color == Piece.BLACK && from_y == to_y - 1 && Board.chessBoardArray[to_x][to_y] != null && Board.chessBoardArray[to_x][to_y].color == Piece.WHITE) isAvailable = true;
+            if (color == Piece.WHITE && from_y == to_y + 1 && board.chessBoardArray[to_x][to_y] != null && board.chessBoardArray[to_x][to_y].color == Piece.BLACK) isAvailable = true;
+            else if (color == Piece.BLACK && from_y == to_y - 1 && board.chessBoardArray[to_x][to_y] != null && board.chessBoardArray[to_x][to_y].color == Piece.WHITE) isAvailable = true;
         } else {
             return false;
         }
 
+        if (isAvailable) hasMoved = true;
         return isAvailable;
     }
 }
